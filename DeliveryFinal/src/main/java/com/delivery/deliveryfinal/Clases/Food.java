6e7b43ad3f74e890ffd335027ebb490b4e4521f4
Sandra Rodriguez <srodriguezg8@upao.edu.pe>
2023-04-23 22:10:21 -0500

    feat: Crear clase Food con los atributos nombre, descripcion y precio

---
+++ DeliveryFinal/src/main/java/com/delivery/deliveryfinal/Clases/Food.java
@@ -0,0 +1,33 @@
+package com.delivery.deliveryfinal.Clases;
+
+public class Food {
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
+
---
+++ DeliveryFinal/src/main/java/com/delivery/deliveryfinal/DeliveryFinalApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.deliveryfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryFinalApplication.class, args);
+    }
+
+}
